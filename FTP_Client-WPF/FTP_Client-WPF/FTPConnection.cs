9454112f9488aa3763a45eac013bf1e6ed943055
Mircea-Stefan Ghideu <shtephan10@gmail.com>
2011-11-02 17:22:46 +0200

    Added FTPConnection class that will give the backend functionality of the application

---
+++ FTP_Client-WPF/FTP_Client-WPF/FTPConnection.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace FTP_Client_WPF
+{
+    class FTPConnection
+    {
+        private String m_ftpUrl;
+        private String m_ftpUserName;
+        private String m_ftpUserPassword;
+
+        public void FTPConnection(String url_val, String username_val, String password_val)
+        {
+            m_ftpUrl = url_val;
+            m_ftpUserName = username_val;
+            m_ftpUserPassword = password_val;
+        }
+
+        public void Login()
+        {
+
+        }
+
+        public void ListContents()
+        {
+
+        }
+
+        public void Download()
+        {
+
+        }
+
+        public void Upload()
+        {
+
+        }
+
+        public void Delete()
+        {
+
+        }
+
+        public void Rename()
+        {
+
+        }
+
+        public void CreateDirectory()
+        {
+
+        }
+    }
+}
